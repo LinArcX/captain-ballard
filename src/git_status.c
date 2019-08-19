70266073ad8994f6722054a29661f745e2587f7b
linarcx <linarcx@riseup.net>
2019-08-19 12:24:17 +0430

    Finally works

--- src/git_status.c
+++ src/git_status.c
@@ -2,41 +2,45 @@
 #include "git_status.h"
 
 int project_status(git_repository *repo, char **files) {
-    git_status_list *status;
-    if (git_repository_is_bare(repo)){
-        fatal("Cannot report status on bare repository", git_repository_path(repo));
-    }
-    struct opts o = {GIT_STATUS_OPTIONS_INIT, "."};
-    o.statusopt.show = GIT_STATUS_SHOW_INDEX_AND_WORKDIR;
-    o.statusopt.flags = GIT_STATUS_OPT_INCLUDE_UNTRACKED | GIT_STATUS_OPT_RENAMES_HEAD_TO_INDEX | GIT_STATUS_OPT_SORT_CASE_SENSITIVELY;
+  git_status_list *status;
+  if (git_repository_is_bare(repo)) {
+    fatal("Cannot report status on bare repository", git_repository_path(repo));
+  }
+  struct opts o = {GIT_STATUS_OPTIONS_INIT, "."};
+  o.statusopt.show = GIT_STATUS_SHOW_INDEX_AND_WORKDIR;
+  o.statusopt.flags = GIT_STATUS_OPT_INCLUDE_UNTRACKED |
+                      GIT_STATUS_OPT_RENAMES_HEAD_TO_INDEX |
+                      GIT_STATUS_OPT_SORT_CASE_SENSITIVELY;
 show_status:
-    check_lg2(git_status_list_new(&status, repo, &o.statusopt), "Could not get status", NULL);
-    print_short(repo, status, files);
-    git_status_list_free(status);
-    //  if (o.repeat)
-    //    printf("\033[H\033[2J");
-    //  if (o.repeat) {
-    //    sleep(o.repeat);
-    //    goto show_status;
-    //  }
-    return 0;
+  check_lg2(git_status_list_new(&status, repo, &o.statusopt),
+            "Could not get status", NULL);
+  print_short(repo, status, files);
+  git_status_list_free(status);
+  //  if (o.repeat)
+  //    printf("\033[H\033[2J");
+  //  if (o.repeat) {
+  //    sleep(o.repeat);
+  //    goto show_status;
+  //  }
+  return 0;
 }
 
-static void print_short(git_repository *repo, git_status_list *status, char **files) {
-    size_t i, maxi = git_status_list_entrycount(status);
-    const git_status_entry *s;
-    const char *c;
+static void print_short(git_repository *repo, git_status_list *status,
+                        char **files) {
+  size_t i, maxi = git_status_list_entrycount(status);
+  const git_status_entry *s;
+  const char *c;
 
-    for (i = 0; i < maxi; ++i) {
-        s = git_status_byindex(status, i);
-        if (s->status == GIT_STATUS_CURRENT)
-            continue;
-        c = NULL;
-        if (s->index_to_workdir) {
-            c = s->index_to_workdir->new_file.path;
-        }
-        vector_push_back(files, (char *)c);
+  for (i = 0; i < maxi; ++i) {
+    s = git_status_byindex(status, i);
+    if (s->status == GIT_STATUS_CURRENT)
+      continue;
+    c = NULL;
+    if (s->index_to_workdir) {
+      c = s->index_to_workdir->new_file.path;
     }
+    vector_push_back(files, (char *)c);
+  }
 }
 
 //  for (i = 0; i < maxi; ++i) {
